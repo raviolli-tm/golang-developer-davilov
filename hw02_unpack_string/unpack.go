bda229ffc295a99cf651d38656ee04e7534a781c
davilov <davilov@diasoft.ru>
2025-04-10 01:43:19 +0300

    Исходный код для задания 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(x string) (string, error) {
+
+	var runes = []rune(x)
+	var flag bool = false
+	var b strings.Builder
+
+	for i := 0; i < len(runes); i++ {
+		if unicode.IsNumber(runes[i]) {
+			if !flag {
+				return "", ErrInvalidString
+			} else {
+				b.WriteString(strings.Repeat(string(runes[i-1]), int(runes[i]-'0')))
+				flag = false
+			}
+		} else if runes[i] == '\\' {
+
+			if flag {
+				b.WriteRune(runes[i-1])
+			}
+			i++
+			if i < len(runes) && (unicode.IsNumber(runes[i]) || runes[i] == '\\') {
+				flag = true
+			} else {
+				return "", ErrInvalidString
+			}
+
+		} else {
+			if flag {
+				b.WriteRune(runes[i-1])
+			}
+			flag = true
+		}
+
+	}
+	if flag {
+		b.WriteRune(runes[len(runes)-1])
+	}
+	return b.String(), nil
 }
