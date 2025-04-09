53cb2c40a276ef8239b3437ca4b5f9bb40836b01
davilov <davilov@diasoft.ru>
2025-04-10 02:19:24 +0300

    Исправление ошибок линтера 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,21 +9,20 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(x string) (string, error) {
-
-	var runes = []rune(x)
-	var flag bool = false
+	runes := []rune(x)
+	var flag bool
 	var b strings.Builder
 
 	for i := 0; i < len(runes); i++ {
-		if unicode.IsNumber(runes[i]) {
+		switch {
+		case unicode.IsNumber(runes[i]):
 			if !flag {
 				return "", ErrInvalidString
-			} else {
-				b.WriteString(strings.Repeat(string(runes[i-1]), int(runes[i]-'0')))
-				flag = false
 			}
-		} else if runes[i] == '\\' {
+			b.WriteString(strings.Repeat(string(runes[i-1]), int(runes[i]-'0')))
+			flag = false
 
+		case runes[i] == '\\':
 			if flag {
 				b.WriteRune(runes[i-1])
 			}
@@ -33,14 +32,12 @@ func Unpack(x string) (string, error) {
 			} else {
 				return "", ErrInvalidString
 			}
-
-		} else {
+		default:
 			if flag {
 				b.WriteRune(runes[i-1])
 			}
 			flag = true
 		}
-
 	}
 	if flag {
 		b.WriteRune(runes[len(runes)-1])
