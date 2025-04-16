cd1796591ed119b5bfaa3caf9abdc978828a5cb6
davilov <davilov@diasoft.ru>
2025-04-16 21:07:47 +0300

    Исходный код для задания 4 без звездочки

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,6 +2,11 @@ package hw04lrucache
 
 type Key string
 
+type val struct {
+	A Key
+	B interface{}
+}
+
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
@@ -9,8 +14,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +25,39 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	var ok bool
+	item, ok := c.items[key]
+	if ok {
+		c.queue.MoveToFront(item)
+		item.Value = val{key, value}
+		return ok
+	}
+	if c.capacity == c.queue.Len() {
+		delete(c.items, (*item).Value.(val).A)
+		c.queue.Remove(c.queue.Back())
+	}
+
+	c.items[key] = c.queue.PushFront(val{key, value})
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	var ok bool
+	var item *ListItem
+	item, ok = c.items[key]
+	if ok {
+		c.queue.MoveToFront(item)
+		return item.Value.(val).B, ok
+	}
+	return nil, false
+
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -17,7 +17,7 @@ func TestList(t *testing.T) {
 
 	t.Run("complex", func(t *testing.T) {
 		l := NewList()
-
+		// [20] - > [10, 20]
 		l.PushFront(10) // [10]
 		l.PushBack(20)  // [10, 20]
 		l.PushBack(30)  // [10, 20, 30]
@@ -25,7 +25,9 @@ func TestList(t *testing.T) {
 
 		middle := l.Front().Next // 20
 		l.Remove(middle)         // [10, 30]
-		require.Equal(t, 2, l.Len())
+		l.Remove(l.Front())
+		l.Remove(l.Front())
+		require.Equal(t, 0, l.Len())
 
 		for i, v := range [...]int{40, 50, 60, 70, 80} {
 			if i%2 == 0 {
@@ -35,17 +37,24 @@ func TestList(t *testing.T) {
 			}
 		} // [80, 60, 40, 10, 30, 50, 70]
 
-		require.Equal(t, 7, l.Len())
+		require.Equal(t, 5, l.Len())
 		require.Equal(t, 80, l.Front().Value)
 		require.Equal(t, 70, l.Back().Value)
+		a := l.Back()
+		l.MoveToFront(a)
+		l.MoveToFront(a) // [80, 60, 40, 10, 30, 50, 70]
+		// [70, 80, 60, 40, 10, 30, 50]
 
-		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
-		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
-
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
+		elems := make([]int, l.Len())
+		elems2 := make([]int, l.Len())
+		for j, i := 0, l.Front(); i != nil; j, i = j+1, i.Next {
+			elems[j] = i.Value.(int)
 		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+		for j, i := l.Len()-1, l.Back(); i != nil; i, j = i.Prev, j-1 {
+			elems2[j] = i.Value.(int)
+		}
+
+		require.Equal(t, elems2, elems)
+		require.Equal(t, []int{70, 80, 60, 40, 50}, elems)
 	})
 }
