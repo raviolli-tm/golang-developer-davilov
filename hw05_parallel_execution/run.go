d4f801cf31961632a339a6875e4ba484447e698f
davilov <davilov@diasoft.ru>
2025-04-19 19:55:27 +0300

    исправленный исходный код для Задания 5

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,67 +2,44 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	var wg sync.WaitGroup
-	intChan := make(chan int)
-	errorChan := make(chan error)
-	stopChan := make(chan struct{})
+	wg.Add(n)
+	lenTask := int32(len(tasks))
+	var taskCount int32
+	var errCount int32
 
 	for i := 0; i < n; i++ {
-		wg.Add(1)
-		go func(stopChan <-chan struct{}) {
+		go func() {
 			defer wg.Done()
 			for {
-				select {
-				case <-stopChan:
+				if int32(m) <= atomic.LoadInt32(&errCount) {
 					return
-				case idx := <-intChan:
-					errorChan <- tasks[idx]()
+				}
+				idx := atomic.AddInt32(&taskCount, 1)
+				if idx <= lenTask {
+					err := tasks[idx-1]()
+					if err != nil {
+						atomic.AddInt32(&errCount, 1)
+					}
+				} else {
+					return
 				}
 			}
-
-		}(stopChan)
+		}()
 	}
-
-	go func() {
-		for i := 0; i < len(tasks); i++ {
-			select {
-			case <-stopChan:
-				return
-			default:
-				intChan <- i
-			}
-
-		}
-		close(stopChan)
-	}()
-
-	go func() {
-		for err := range errorChan {
-			fmt.Println("err", m, err)
-			if err != nil {
-				m--
-				if m == 0 {
-					close(stopChan)
-				}
-			}
-		}
-	}()
 
 	wg.Wait()
-	close(errorChan)
-	if m > 0 {
-		return nil
+	if errCount > int32(m) {
+		return ErrErrorsLimitExceeded
 	}
-	return ErrErrorsLimitExceeded
-
+	return nil
 }
