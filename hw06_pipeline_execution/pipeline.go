563a3b690887c1f62deb05ccc845aae040a85195
davilov <davilov@diasoft.ru>
2025-04-23 00:28:52 +0300

    Исходный код для задания 6

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,37 @@ type (
 
 type Stage func(in In) (out Out)
 
+func ExecutePipeline2(in In, done In, stages ...Stage) Out {
+
+	if len(stages) == 1 {
+		return stages[0](in)
+	}
+
+	return ExecutePipeline2(stages[0](in), done, stages[1:]...)
+
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	preparedOut := make(Bi)
+	outChan := stages[0](in)
+	channel := ExecutePipeline2(outChan, done, stages[1:]...)
+
+	go func(out Out, preparedOut Bi) {
+		defer close(preparedOut)
+		for {
+			select {
+			case v, ok := <-out:
+				if !ok {
+					return
+				}
+				preparedOut <- v
+			case <-done:
+				return
+
+			}
+		}
+	}(channel, preparedOut)
+
+	return preparedOut
+
 }
