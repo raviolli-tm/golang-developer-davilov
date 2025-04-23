6315865eeba703bd814d822b143eea7e07ba4cfa
davilov <davilov@diasoft.ru>
2025-04-23 17:18:56 +0300

    Исправление ошибок линтера

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,13 +9,11 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline2(in In, done In, stages ...Stage) Out {
-
 	if len(stages) == 1 {
 		return stages[0](in)
 	}
 
 	return ExecutePipeline2(stages[0](in), done, stages[1:]...)
-
 }
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
@@ -34,11 +32,9 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 				preparedOut <- v
 			case <-done:
 				return
-
 			}
 		}
 	}(channel, preparedOut)
 
 	return preparedOut
-
 }
