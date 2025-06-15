97f3c09bcd5ab9de11cd779d3c6b48c7ce2acb7f
raviolli-tm <workbook2009@yandex.ru>
2025-06-15 09:14:14 +0300

    add source code to task 7 - hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,59 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
+	ErrFilePathIsRequired    = errors.New("filepath is required")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	if fromPath == "" || toPath == "" {
+		return ErrFilePathIsRequired
+	}
+
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	size, err := sourceFile.Seek(0, 2)
+	defer sourceFile.Close()
+
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	if size < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	sourceFile.Seek(offset, 0)
+	targetFile, _ := os.Create(toPath)
+	defer targetFile.Close()
+	var n int64
+
+	if size-offset < limit || limit == 0 {
+		limit = size - offset
+	}
+
+	bar := pb.New64(limit).Start()
+	barReader := bar.NewProxyReader(sourceFile)
+	n, err = io.CopyN(targetFile, barReader, limit)
+	bar.Finish()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%d bytes copied from %s to %s\n", n, from, to)
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,25 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+func TestCopyErrors(t *testing.T) {
+	t.Run("Check errors", func(t *testing.T) {
+		err := Copy("", "a.txt", 10, 10)
+		if err != nil {
+			assert.ErrorIs(t, err, ErrFilePathIsRequired)
+		}
+
+		err = Copy("a.txt", "", 10, 10)
+		if err != nil {
+			assert.ErrorIs(t, err, ErrFilePathIsRequired)
+		}
+
+		err = Copy("testdata/input.txt", "out.txt", 20000, 10)
+		if err != nil {
+			assert.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+		}
+	})
 }
