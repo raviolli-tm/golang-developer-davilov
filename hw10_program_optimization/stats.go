8ca5ee9d5a2faf5415a5216fef0f224f6f4afca3
raviolli-tm <workbook2009@yandex.ru>
2025-07-24 22:19:38 +0300

    fix linter's errors

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -12,7 +12,6 @@ type DomainStat map[string]int
 var domainRegex = regexp.MustCompile(`@[a-zA-Z0-9\.]*`)
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-
 	result := make(DomainStat)
 	byteDomain := []byte(domain)
 	scanner := bufio.NewScanner(r)
@@ -20,15 +19,11 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	for scanner.Scan() {
 		line := scanner.Bytes()
 		domainFind := domainRegex.Find(line)
-
 		if bytes.HasSuffix(domainFind, byteDomain) {
-
 			res := bytes.ToLower(domainFind[1:])
 			result[string(res)]++
-
 		}
 	}
 
 	return result, nil
-
 }
