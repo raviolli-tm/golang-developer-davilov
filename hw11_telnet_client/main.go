4b6fceca08b77f5fb17c9cf4501d51c811bfec89
raviolli-tm <workbook2009@yandex.ru>
2025-07-31 23:33:43 +0300

    task 11 - source code

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,63 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		log.Fatal("Usage: go run main.go --timeout=<timeout> <host> <port>")
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill, syscall.SIGINT)
+	defer cancel()
+
+	client := NewTelnetClient(fmt.Sprintf("%s:%s", host, port), timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatalf("Unable to connect by tcp, %v", err)
+	}
+
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			log.Printf("Unable to receive from tcp, %v \n", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			err := client.Close()
+			if err != nil {
+				log.Fatalf("Unable to close tcp connection, %v", err)
+			}
+		default:
+			err = client.Send()
+			if err != nil {
+				cancel()
+			}
+		}
+
+	}
+
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,73 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TelnetClientImpl struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	tcpIn   *bufio.Reader
+	tcpOut  *bufio.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &TelnetClientImpl{address: address, timeout: timeout, in: in, out: out}
+}
+
+func (s *TelnetClientImpl) Connect() error {
+
+	if s.conn != nil {
+		return nil
+	}
+	dial, err := net.DialTimeout("tcp", "localhost:4242", s.timeout)
+	if err != nil {
+		return err
+	}
+	s.conn = dial
+	s.tcpIn = bufio.NewReader(s.conn)
+	s.tcpOut = bufio.NewWriter(s.conn)
+
+	return nil
+}
+func (s *TelnetClientImpl) Close() error {
+	err := s.conn.Close()
+	if err != nil {
+		return err
+	}
+	err = s.in.Close()
+	if err != nil {
+		return err
+	}
+	s.conn = nil
+	s.tcpIn = nil
+	s.tcpOut = nil
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (s *TelnetClientImpl) Send() error {
+	if s.conn == nil {
+		return fmt.Errorf("no connection")
+	}
+	if s.in == nil {
+		return fmt.Errorf("input stream is not set")
+	}
+	_, err := io.Copy(s.tcpOut, s.in)
+	if err != nil {
+
+		return err
+	}
+
+	return s.tcpOut.Flush()
+}
+func (s *TelnetClientImpl) Receive() error {
+	if s.conn == nil {
+		return fmt.Errorf("no connection")
+	}
+	if s.out == nil {
+		return fmt.Errorf("output stream is not set")
+	}
+	_, err := io.Copy(s.out, s.tcpIn)
+	return err
+
+}
